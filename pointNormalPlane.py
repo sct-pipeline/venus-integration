7ab1a920633b6b0ad77a800d6165973a5dc6a271
NadiaBlostein <nadia.blostein@mail.mcgill.ca>
2022-11-15 15:52:13 -0500

    Class to represent point normal plane as json file

---
+++ pointNormalPlane.py
@@ -0,0 +1,35 @@
+
+import json
+
+class pointNormalPlane(object):
+    """
+    Class to represent point-Normal plane (that can be written to and from a .json file)
+
+    :param origin: list [x,y,z] with origin coordinates (float or int)
+    :param normal: list [a,b,c] with normal components (float or int)
+    :param orientation: 3-character string (e.g. 'RAS','LPS','LPI')
+    :param filename: string ending in .json
+    :param space: 'SCT_image' or 'anatomical'
+    """
+    def __init__(self,origin,normal,orientation,space='SCT_image'):
+        self.origin = origin
+        self.normal = normal
+        self.orientation = orientation
+        self.space = space
+
+    @classmethod
+    def fromJsonFile(cls,filename):
+        with open(filename, "r") as f:
+            data=f.read()
+        dict_tmp=json.loads(data)
+        return(pointNormalPlane(origin=dict_tmp['origin'],normal=dict_tmp['normal'],orientation=dict_tmp['orientation'],space=dict_tmp['space']))
+
+    def write_plane_json(self,filename):
+        new_plane_dict_json = json.dumps(self.__dict__)
+        if (filename.endswith('json')):
+            with open(filename, "w") as f:
+                f.write(new_plane_dict_json)
+        else:
+            with open(f'{filename}.json', "w") as f:
+                f.write(new_plane_dict_json)
+
