c6f2bbc3584ef3e8eb824de6a3970c74f12471c8
NadiaBlostein <nadia.blostein@mail.mcgill.ca>
2022-11-16 13:01:05 -0500

    used by slice_select in order to create a point-normal plane markup file that 3D Slicer can read

---
+++ write_slicer_markup_json.py
@@ -0,0 +1,25 @@
+
+import os
+
+fname_out =sys.argv[1]
+origin = [float(sys.argv[2]),float(sys.argv[3]),float(sys.argv[4])]
+normal = [float(sys.argv[5]),float(sys.argv[6]),float(sys.argv[7])]
+
+slicer.util.loadScene('2022-11-16-Scene.mrml')
+input_Node = getNode("input-pointNormal-Plane-markup")
+print(f'input plane normal: {input_Node.GetNormal()}')
+print(f'input plane normal: {input_Node.GetOrigin()}')
+print('\n')
+input_Node.SetNormal(normal)
+input_Node.SetOrigin(origin)
+print('New plane values:')
+print(f'new plane normal: {input_Node.GetNormal()}')
+print(f'new plane origin: {input_Node.GetOrigin()}')
+myStorageNode = input_Node.CreateDefaultStorageNode()
+
+filename=f'{os.getcwd()}/{fname_out}'
+print(f'filename: {filename}')
+myStorageNode.SetFileName(filename)
+myStorageNode.WriteData(input_Node)
+
+sys.exit(0)
